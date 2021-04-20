a00578eeb162007bcf42c8e3216e5b8b966270c1
sora-sea <yasu.abc.jda@gmail.com>
2021-04-20 15:59:01 +0900

    修正しました。

--- js/main.js
+++ js/main.js
@@ -2,11 +2,11 @@
 
 {
   for (let i = 1; i <= 99; i++) {
-    if (i % 3 == 0 && i % 5 == 0)
+    if (i % 3 === 0 && i % 5 === 0)
       console.log(`FizzBuzz ${i}`);
-    else if (i % 3 == 0)
+    else if (i % 3 === 0)
       console.log(`Fizz ${i}`);
-    else if (i % 5 == 0)
+    else if (i % 5 === 0)
       console.log(`Buzz ${i}`);
     else console.log()
   }
